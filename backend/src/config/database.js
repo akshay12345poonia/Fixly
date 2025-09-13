489c0ac7f4e5173161b51460995a401a008b0558
akshay Poonia <akshay12345poonia@gmail.com>
2025-09-13 23:33:31 +0530

    fix

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -3,9 +3,22 @@ const mongoose = require('mongoose');
 
 function connectDB() {
   const uri = process.env.MONGODB_URI;
-  mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+
+  mongoose.connect(uri)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error('DB connection error:', err));
 }
 
 module.exports = connectDB;
+
+// // backend/src/config/database.js
+// const mongoose = require('mongoose');
+
+// function connectDB() {
+//   const uri = process.env.MONGODB_URI;
+//   mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(() => console.log('MongoDB connected'))
+//     .catch(err => console.error('DB connection error:', err));
+// }
+
+// module.exports = connectDB;
